ce1493767b89ccd994c9b18d60e3b241bbd0f4fe
Victor S Pacheco <victorspacheco2004@gmail.com>
2026-06-08 09:11:20 -0300

    fix: PWM adjustment

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -107,6 +107,9 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
+  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_4, 160); /*Duty Cycle de 20% para 10KHz*/
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -222,9 +225,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 7999;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 499;
+  htim2.Init.Period = 799;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
